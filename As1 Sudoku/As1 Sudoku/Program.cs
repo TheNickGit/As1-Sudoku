6781a8d8b4cbb7e53101887e5511b3f61a67ed18
TheNickTestGit <74415397+TheNickTestGit@users.noreply.github.com>
2021-12-07 13:50:31 +0100

    Initial Commit

---
+++ As1 Sudoku/As1 Sudoku/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace As1_Sudoku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
